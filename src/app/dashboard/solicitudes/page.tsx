4ff54bf634b38125d8ecde8c1e9d976433428b21
clanie1 <danybarociov@gmail.com>
2023-04-01 21:02:54 -0700

    filter section

--- src/app/dashboard/solicitudes/page.tsx
+++ src/app/dashboard/solicitudes/page.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { AiOutlineUnorderedList } from "react-icons/ai";
 import SolicitudDiv from "./SolicitudDiv";
+import { GoSettings } from "react-icons/go";
 
 type Props = {};
 
@@ -13,8 +14,20 @@ const Solicitudes = (props: Props) => {
         <div className="text-xl">Solicitudes</div>
       </div>
       <div className=" overflow-y-auto w-[92%] m-auto flex flex-col gap-2  px-2 h-full">
-        <h1 className="m-auto">filters</h1>
-        <div></div>
+        <div className="flex justify-between px-10 mt-7 items-center">
+          <div className="flex gap-4 items-center">
+            <div className="p-2 hover:bg-black duration-[70ms] hover:text-white cursor-pointer rounded-lg border-2">
+              <GoSettings className="w-7 h-7" />
+            </div>
+            <label className="text-xl font-bold">Todas</label>
+          </div>
+          <div className="flex gap-2">
+            <div className="rounded-full bg-orange-400 w-5 h-5 flex items-center justify-center text-white p-3">
+              <div>2</div>
+            </div>
+            <label>solicitudes</label>
+          </div>
+        </div>
         {/* assets */}
         <div className=" m-auto h-full w-full py-4 gap-4 flex flex-col">
           <SolicitudDiv />
