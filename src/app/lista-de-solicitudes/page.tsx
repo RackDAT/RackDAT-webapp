9c3d52ec1da90c1716581273f6931ec1311bfc1b
Adrián Fernández <oscar.adrian.fersa@gmail.com>
2023-04-02 20:35:42 -0700

    literally just centered a div lmao

--- src/app/lista-de-solicitudes/page.tsx
+++ src/app/lista-de-solicitudes/page.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
+import Card from './solicitudesCard';
 
-const About = () => {
+const ListaSolicitudes = () => {
   return (
-    <div>
-      <h1>About Page</h1>
+    <div className='p-2.5 bg-white'>
+      <div>hola</div>
+      <div className='flex items-center justify-center h-screen'>
+        <Card />
+      </div>
     </div>
   );
 };
 
-export default About;
+export default ListaSolicitudes;
---
+++ src/app/lista-de-solicitudes/solicitudesCard.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Card = () => {
+  return (
+    <div className='w-11/12 bg-gray-200 h-44 rounded-lg'>
+      hola
+    </div>
+  );
+};
+
+export default Card;
