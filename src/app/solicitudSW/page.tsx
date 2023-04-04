35cb3e930e13b326725db08a695eae82b3b18dee
Osminalx <verdestro64@gmail.com>
2023-04-03 18:44:50 -0700

    listo

---
+++ src/app/solicitudSW/page.tsx
@@ -0,0 +1,11 @@
+import Solicitud from './SolicitudSWForm'
+
+const solicitud_software = () => {
+  return (
+    <div className='h-screen flex justify-center items-center' >
+        <Solicitud/>
+    </div>
+  )
+}
+
+export default solicitud_software
