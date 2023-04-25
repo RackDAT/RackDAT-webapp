74eeda68c7630e7476c5c9c47559692ed05cf46c
clanie1 <danybarociov@gmail.com>
2023-04-25 09:51:24 -0700

    login logout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,20 +2,28 @@ import Image from "next/image";
 import { Inter } from "next/font/google";
 import styles from "./page.module.css";
 import Btn from "@/components/global/Btn";
-import "./globals.css";
+import { useUser } from "@auth0/nextjs-auth0/client";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  const { user, error, isLoading } = useUser();
+  console.log(user);
   return (
     <div className="w-full h-screen">
       <div className="m-auto w-fit">
         <Btn style="strong">
           <a href="/api/auth/login">LogIn</a>
         </Btn>
-
-        <Btn style="light">SignUp</Btn>
       </div>
+      {user && (
+        <>
+          <h1>hay usuario senores</h1>
+          <a href="/api/auth/logout" className="text-white">
+            logout
+          </a>
+        </>
+      )}
     </div>
   );
 }
