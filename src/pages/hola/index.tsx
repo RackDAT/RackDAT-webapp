8754c1f2fdd8c54d2f0e7fbd7a19cf721e197ed0
clanie1 <danybarociov@gmail.com>
2023-04-25 05:26:09 -0700

    migracion a pages, se rompen los estilos

---
+++ src/pages/hola/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+type Props = {};
+
+const index = (props: Props) => {
+  return <div>hello bby girl</div>;
+};
+
+export default index;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import { Inter } from "next/font/google";
 import styles from "./page.module.css";
 import Btn from "@/components/global/Btn";
+import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
