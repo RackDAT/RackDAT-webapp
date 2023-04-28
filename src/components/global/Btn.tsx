c9252abb135b401882f7a81d78ad8a3c28c6a83b
Adrián Fernández <oscar.adrian.fersa@gmail.com>
2023-04-27 23:35:44 -0700

    finished view, need to implement endpoints

--- src/components/global/Btn.tsx
+++ src/components/global/Btn.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 type Props = {
-  style: "strong" | "light";
+  style: "strong" | "light" | "dark";
   children: React.ReactNode;
 };
 
@@ -9,6 +9,7 @@ const Btn = (props: Props) => {
   const styles = {
     strong: "bg-orange-400 text-white hover:bg-orange-300",
     light: "bg-orange-100 text-orange-400 hover:bg-orange-200",
+    dark: "bg-black text-white border hover:bg-white hover:text-black hover:border hover:border-black",
   };
   return (
     <button className={"px-7 py-2  rounded-md " + styles[props.style]}>
--- src/pages/dashboard/userlist/index.tsx
+++ src/pages/dashboard/userlist/index.tsx
@@ -1,34 +1,50 @@
 "use client";
 
-import React from "react";
-import { AiOutlineUnorderedList } from "react-icons/ai";
+import React, { useState } from "react";
 import UserDiv from "./userdiv";
-import { GoSettings } from "react-icons/go";
+import { FaUserCircle } from "react-icons/fa";
+import { AiOutlineSearch } from "react-icons/ai";
 import Layout from "../layout";
+import Btn from "@/components/global/Btn";
 type Props = {};
 
 const Solicitudes = (props: Props) => {
+  const [search, setSearch] = useState("");
+
+  const handleChange = (event: any) => {
+    setSearch(event.target.value);
+  };
+
   return (
     <Layout>
       <div className="flex flex-col">
         {/* header */}
-        <div className="border-b-2 border-neutral-300 w-full py-4 px-4 flex items-center gap-2 sticky top-0 z-20 bg-[#F5F5F5]">
-          <AiOutlineUnorderedList className="w-6 h-6" />
-          <div className="text-xl">Solicitudes</div>
+        <div className="border-b-2 border-neutral-300 w-full py-4 px-4 flex items-center gap-2 sticky top-0 z-20 bg-[#F5F5F5] justify-between">
+          <div className="flex gap-2">
+            <FaUserCircle className="w-6 h-6" />
+            <div className="text-xl">Usuarios</div>
+          </div>
+          <div className="flex justify-end">
+            <div className="relative p-2">
+              <Btn style="dark">Nuevas solicitudes</Btn>
+            </div>
+            <div className="rounded-full bg-orange-400 w-5 h-5 flex items-center justify-center text-white p-3 absolute">
+              2
+            </div>
+          </div>
         </div>
         <div className=" overflow-y-auto w-[92%] m-auto flex flex-col gap-2  px-2 h-full">
           <div className="flex justify-between px-10 mt-7 items-center">
-            <div className="flex gap-4 items-center">
-              <div className="p-2 hover:bg-black duration-[70ms] hover:text-white cursor-pointer rounded-lg border-2">
-                <GoSettings className="w-7 h-7" />
-              </div>
-              <label className="text-xl font-bold">Todas</label>
-            </div>
-            <div className="flex gap-2">
-              <div className="rounded-full bg-orange-400 w-5 h-5 flex items-center justify-center text-white p-3">
-                <div>2</div>
-              </div>
-              <label>solicitudes</label>
+            <div className="flex items-center justify-start bg-gray-200 text-gray-600 h-7 w-1/4 rounded-lg p-3 gap-2 hover:bg-gray-300 hover:scale-[100.5%] duration-200 hover:shadow-md">
+              <AiOutlineSearch className="w-5 h-5"></AiOutlineSearch>
+              <form>
+                <input
+                  type="text"
+                  value={search}
+                  placeholder="Buscar"
+                  className="bg-transparent w-fit"
+                />
+              </form>
             </div>
           </div>
           {/* assets */}
