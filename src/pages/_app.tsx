b6c7dbc06ab843bdc39d5435d2fff481417c7121
clanie1 <danybarociov@gmail.com>
2023-05-04 20:23:17 -0700

    routing loading

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,8 +2,26 @@ import React, { ReactComponentElement } from "react";
 import { UserProvider } from "@auth0/nextjs-auth0/client";
 import "./globals.css";
 import { AppProps } from "next/app";
+import "nprogress/nprogress.css";
+import NProgress from "nprogress";
+import { useEffect } from "react";
 
-export default function MyApp({ Component, pageProps }: AppProps) {
+export default function MyApp({ Component, pageProps, router }: AppProps) {
+  React.useEffect(() => {
+    const handleRouteStart = () => NProgress.start();
+    const handleRouteDone = () => NProgress.done();
+
+    router.events.on("routeChangeStart", handleRouteStart);
+    router.events.on("routeChangeComplete", handleRouteDone);
+    router.events.on("routeChangeError", handleRouteDone);
+
+    return () => {
+      // Make sure to remove the event handler on unmount!
+      router.events.off("routeChangeStart", handleRouteStart);
+      router.events.off("routeChangeComplete", handleRouteDone);
+      router.events.off("routeChangeError", handleRouteDone);
+    };
+  }, []);
   return (
     <UserProvider>
       <Component {...pageProps} />
