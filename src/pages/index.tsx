4effe18682077e85fe2ecea65742dd60f025d7c6
Adrián Fernández <oscar.adrian.fersa@gmail.com>
2023-05-11 20:55:33 -0700

    don't check this

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,23 +7,5 @@ import { useUser } from "@auth0/nextjs-auth0/client";
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  const { user, error, isLoading } = useUser();
-  console.log(user);
-  return (
-    <div className="w-full h-screen">
-      {/* <div className="m-auto w-fit">
-        <Btn style="strong">
-          <a href="/api/auth/login">LogIn</a>
-        </Btn>
-      </div>
-      {user && (
-        <>
-          <h1>hay usuario senores</h1>
-          <a href="/api/auth/logout" className="text-white">
-            logout
-          </a>
-        </>
-      )} */}
-    </div>
-  );
+  return <div className="w-full h-screen"></div>;
 }
